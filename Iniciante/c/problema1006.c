6d60e30170c3d87a17bc14c8e7527ea7bd8daf08
Marcela Silvério <marcelapsilverio@gmail.com>
2021-05-14 22:11:39 -0300

    Stop point: 1008

--- Iniciante/c/problema1006.c
+++ Iniciante/c/problema1006.c
@@ -8,8 +8,15 @@
 
 int main(){
 
-    double nota1, nota2, nota3, media;
+    double nota1, nota2, nota3, media; //declaracao das variaveis double
 
+    scanf("%lf", &nota1); //leitura da primeira nota
+    scanf("%lf", &nota2); //leitura da segunda nota
+    scanf("%lf", &nota3); //leitura da terceira nota
+
+    media = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3))/(peso1 + peso2 + peso3); //calculo da media
+
+    printf("MEDIA = %0.1lf\n", media); //impressao de dados
 
     return 0;
 
